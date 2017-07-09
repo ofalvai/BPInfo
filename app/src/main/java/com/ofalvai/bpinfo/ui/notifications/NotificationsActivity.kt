6d5dfd0459fcc0c1a47736719945c02b71959220
Olivér Falvai <ofalvai@gmail.com>
2017-07-09 18:02:52 +0200

    Added test TabLayout

--- app/src/main/java/com/ofalvai/bpinfo/ui/notifications/NotificationsActivity.kt
+++ app/src/main/java/com/ofalvai/bpinfo/ui/notifications/NotificationsActivity.kt
@@ -3,12 +3,18 @@ package com.ofalvai.bpinfo.ui.notifications
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.iid.FirebaseInstanceId
 import com.ofalvai.bpinfo.R
+import com.ofalvai.bpinfo.util.bindView
+import timber.log.Timber
 
 class NotificationsActivity : AppCompatActivity(), NotificationsContract.View {
 
     lateinit var mPresenter: NotificationsContract.Presenter
+
+    val mTabLayout: TabLayout by bindView(R.id.notifications__tabs)
 
     companion object {
         @JvmStatic
@@ -23,5 +29,15 @@ class NotificationsActivity : AppCompatActivity(), NotificationsContract.View {
 
         mPresenter = NotificationsPresenter()
         mPresenter.attachView(this)
+
+        Timber.d("FCM token: " + FirebaseInstanceId.getInstance().token)
+
+        mTabLayout.addTab(mTabLayout.newTab().setText("Busz"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Metró"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Villamos"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Trolibusz"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("HÉV"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Hajó"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Egyéb"))
     }
 }
