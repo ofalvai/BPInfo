8d1181d39d7fdd780fb94a2ab1525e17dbf93422
Olivér Falvai <ofalvai@gmail.com>
2018-02-02 22:49:37 +0100

    Re-enabled notifications menu item

--- app/src/main/java/com/ofalvai/bpinfo/ui/alertlist/AlertListFragment.kt
+++ app/src/main/java/com/ofalvai/bpinfo/ui/alertlist/AlertListFragment.kt
@@ -38,6 +38,7 @@ import com.ofalvai.bpinfo.ui.alert.AlertDetailFragment
 import com.ofalvai.bpinfo.ui.alertlist.adapter.AlertAdapter
 import com.ofalvai.bpinfo.ui.alertlist.dialog.AlertFilterFragment
 import com.ofalvai.bpinfo.ui.alertlist.dialog.NoticeFragment
+import com.ofalvai.bpinfo.ui.notifications.NotificationsActivity
 import com.ofalvai.bpinfo.ui.settings.SettingsActivity
 import com.ofalvai.bpinfo.util.*
 import kotterknife.bindView
@@ -151,7 +152,7 @@ class AlertListFragment : Fragment(), AlertListContract.View, AlertFilterFragmen
         when (item.itemId) {
             R.id.menu_item_filter_alerts -> displayFilterDialog()
             R.id.menu_item_settings -> startActivity(SettingsActivity.newIntent(context))
-//            R.id.menu_item_notifications -> startActivity(NotificationsActivity.newIntent(context))
+            R.id.menu_item_notifications -> startActivity(NotificationsActivity.newIntent(context))
         }
         return true
     }
