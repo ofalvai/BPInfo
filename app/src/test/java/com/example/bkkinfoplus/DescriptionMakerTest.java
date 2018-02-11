6285933820b4241f7896fc9cec50a8af0fe5eae9
Olivér Falvai <ofalvai@gmail.com>
2018-02-11 19:17:24 +0100

    Fixed and added more test cases for DescriptionMaker

--- app/src/test/java/com/example/bkkinfoplus/DescriptionMakerTest.java
+++ app/src/test/java/com/example/bkkinfoplus/DescriptionMakerTest.java
@@ -23,9 +23,9 @@ import static org.mockito.Mockito.when;
 public class DescriptionMakerTest {
 
     @SuppressLint("StaticFieldLeak")
-    private static Context mContext;
+    private static Context context;
 
-    private Map<String, String> mRouteMap;
+    private Map<String, String> routeMap;
 
     private static final String DATA_KEY_ROUTE_BUS = "route_bus";
     private static final String DATA_KEY_ROUTE_FERRY = "route_ferry";
@@ -37,76 +37,126 @@ public class DescriptionMakerTest {
 
     @BeforeClass
     public static void setUpMocks() {
-        mContext = mock(Context.class);
+        context = mock(Context.class);
         Resources resources = mock(Resources.class);
         Configuration configuration = mock(Configuration.class);
         Locale locale = new Locale.Builder().setLanguage("hu").build();
 
-        when(mContext.getResources()).thenReturn(resources);
+        when(context.getResources()).thenReturn(resources);
         when(resources.getConfiguration()).thenReturn(configuration);
         configuration.locale = locale;
 
-        when(mContext.getString(R.string.route_bus)).thenReturn("Busz");
-        when(mContext.getString(R.string.route_ferry)).thenReturn("Hajó");
-        when(mContext.getString(R.string.route_rail)).thenReturn("HÉV");
-        when(mContext.getString(R.string.route_tram)).thenReturn("Villamos");
-        when(mContext.getString(R.string.route_trolleybus)).thenReturn("Trolibusz");
-        when(mContext.getString(R.string.route_subway)).thenReturn("Metró");
-        when(mContext.getString(R.string.route_other)).thenReturn("Egyéb");
-
+        when(context.getString(R.string.route_bus_alt)).thenReturn("busz");
+        when(context.getString(R.string.route_ferry_alt)).thenReturn("hajó");
+        when(context.getString(R.string.route_rail_alt)).thenReturn("HÉV");
+        when(context.getString(R.string.route_tram_alt)).thenReturn("villamos");
+        when(context.getString(R.string.route_trolleybus_alt)).thenReturn("trolibusz");
+        when(context.getString(R.string.route_subway_alt)).thenReturn("metró");
+        when(context.getString(R.string.route_other)).thenReturn("Egyéb");
     }
 
     @Before
     public void setUpRouteMap() {
-        mRouteMap = new HashMap<>();
-        mRouteMap.put(DATA_KEY_ROUTE_BUS, "");
-        mRouteMap.put(DATA_KEY_ROUTE_FERRY, "");
-        mRouteMap.put(DATA_KEY_ROUTE_RAIL, "");
-        mRouteMap.put(DATA_KEY_ROUTE_TRAM, "");
-        mRouteMap.put(DATA_KEY_ROUTE_TROLLEYBUS, "");
-        mRouteMap.put(DATA_KEY_ROUTE_SUBWAY, "");
-        mRouteMap.put(DATA_KEY_ROUTE_OTHER, "");
+        routeMap = new HashMap<>();
+        routeMap.put(DATA_KEY_ROUTE_BUS, "");
+        routeMap.put(DATA_KEY_ROUTE_FERRY, "");
+        routeMap.put(DATA_KEY_ROUTE_RAIL, "");
+        routeMap.put(DATA_KEY_ROUTE_TRAM, "");
+        routeMap.put(DATA_KEY_ROUTE_TROLLEYBUS, "");
+        routeMap.put(DATA_KEY_ROUTE_SUBWAY, "");
+        routeMap.put(DATA_KEY_ROUTE_OTHER, "");
     }
 
     @Test
-    public void single_Bus() {
-        mRouteMap.put(DATA_KEY_ROUTE_BUS, "7");
+    public void single_bus() {
+        routeMap.put(DATA_KEY_ROUTE_BUS, "7");
 
-        String result = DescriptionMaker.Companion.makeDescription(mRouteMap, mContext);
+        String result = DescriptionMaker.makeDescription(routeMap, context);
 
-        assertEquals("Single bus route is correct", "7-es Busz", result);
+        assertEquals("Single bus route is correct", "7-es busz", result);
     }
 
     @Test
     public void multiple_trams() {
-        mRouteMap.put(DATA_KEY_ROUTE_TRAM, "47|49|56");
+        routeMap.put(DATA_KEY_ROUTE_TRAM, "47|49|56");
 
-        String result = DescriptionMaker.Companion.makeDescription(mRouteMap, mContext);
+        String result = DescriptionMaker.makeDescription(routeMap, context);
 
-        String expected = "47-es, 49-es, 56-os Villamos";
+        String expected = "47-es, 49-es, 56-os villamos";
         assertEquals("Multiple tram routes are concatenated", expected, result);
     }
 
     @Test
+    public void multiple_routeNumbersWithLetterEndings() {
+        routeMap.put(DATA_KEY_ROUTE_TRAM, "12M|14M");
+        routeMap.put(DATA_KEY_ROUTE_BUS, "30A|84E");
+
+        String result = DescriptionMaker.makeDescription(routeMap, context);
+
+        String expected = "30A, 84E busz\n12M, 14M villamos";
+        assertEquals("Numbered routes with letter endings don't have postfixes", expected, result);
+    }
+
+    @Test
     public void multiple_SubwaysAndRails() {
-        mRouteMap.put(DATA_KEY_ROUTE_SUBWAY, "M2|M4");
-        mRouteMap.put(DATA_KEY_ROUTE_RAIL, "H8|H9");
+        routeMap.put(DATA_KEY_ROUTE_SUBWAY, "M2|M4");
+        routeMap.put(DATA_KEY_ROUTE_RAIL, "H8|H9");
 
-        String result = DescriptionMaker.Companion.makeDescription(mRouteMap, mContext);
+        String result = DescriptionMaker.makeDescription(routeMap, context);
 
-        String expected = "M2-es, M4-es Metró\nH8-as, H9-es HÉV";
+        String expected = "M2-es, M4-es metró\nH8-as, H9-es HÉV";
         assertEquals("Multiple tram and rail routes are concatenated, then broken into lines", expected, result);
     }
 
     @Test
     public void error_noRouteKeyInMap() {
-        mRouteMap.remove(DATA_KEY_ROUTE_SUBWAY);
-        mRouteMap.put(DATA_KEY_ROUTE_BUS, "32");
+        routeMap.remove(DATA_KEY_ROUTE_SUBWAY);
+        routeMap.put(DATA_KEY_ROUTE_BUS, "32");
 
-        String result = DescriptionMaker.Companion.makeDescription(mRouteMap, mContext);
+        String result = DescriptionMaker.makeDescription(routeMap, context);
 
-        String expected = "32-es Busz";
+        String expected = "32-es busz";
         assertEquals("No exception shown when a route type is missing, other routes are correct", expected, result);
     }
 
+    @Test
+    public void error_invalidSeparatorInRouteList() {
+        routeMap.put(DATA_KEY_ROUTE_BUS, "32|");
+
+        String result = DescriptionMaker.makeDescription(routeMap, context);
+
+        String expected = "32-es busz";
+        assertEquals("No exception shown when route list ends with separator", expected, result);
+    }
+
+    @Test
+    public void error_invalidSeparatorInRouteList2() {
+        routeMap.put(DATA_KEY_ROUTE_BUS, "|32");
+
+        String result = DescriptionMaker.makeDescription(routeMap, context);
+
+        String expected = "32-es busz";
+        assertEquals("No exception shown when route list starts with separator", expected, result);
+    }
+
+    @Test
+    public void otherTypes() {
+        routeMap.put(DATA_KEY_ROUTE_OTHER, "Libegő|Sikló|BP100|N17|Sétajárat");
+
+        String result = DescriptionMaker.makeDescription(routeMap, context);
+
+        String expected = "Libegő, Sikló, BP100-as, N17-es, Sétajárat";
+        assertEquals("Routes of type OTHER are listed without the route type", expected, result);
+    }
+
+    @Test
+    public void otherTypes_wildShortNames() {
+        routeMap.put(DATA_KEY_ROUTE_OTHER, "❄");
+        routeMap.put(DATA_KEY_ROUTE_BUS, " 7 ");
+
+        String result = DescriptionMaker.makeDescription(routeMap, context);
+
+        String expected = "7-es busz\n❄";
+        assertEquals("Routes of type OTHER are listed without the route type", expected, result);
+    }
 }
